347dec10282fa2aed3fc0c051b567f833eb736c3
gjwwill <guojunwen@sowill.cc>
2018-09-18 09:57:05 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+//app.js
+App({
+  globalData: {
+    prefix: 'https://xcx.sowill.cc:8443',
+    userInfo: null
+  }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,50 @@
+//index.js
+//获取应用实例
+const app = getApp()
+Page({
+  data: {
+    type: 0,
+    title: '选择学校',
+    tab: [{ text: '高教', value: 1 }, { text: '普教', value: 0 }],
+    schoolList: []
+  },
+  onLoad: function () {
+    this.getSchoolInfo()
+    var schoolId = wx.getStorageSync('schoolId') || ''
+    var schoolName = wx.getStorageSync('schoolName') || ''
+    if (schoolId && schoolName) {
+      wx.navigateTo({
+        url: '../school/school?id=' + schoolId + '&name=' + schoolName
+      })
+    }
+  },
+  onHide () {
+    wx.setStorageSync('schoolId', '')
+    wx.setStorageSync('schoolName', '')
+  },
+  getSchoolInfo () {
+    wx.request({
+      url: app.globalData.prefix + '/school/findSchoolList',
+      method: 'POST',
+      data: {
+        type: this.data.type,
+        page: { pageSize: 100, pageIndex: 1 }
+      },
+      header: { 'content-type': 'application/json' },
+      success: res => {
+        this.setData({
+          schoolList: res.data.list
+        })
+      },
+      fail: err => {
+        console.log(err)
+      }
+    })
+  },
+  typeChange: function (e) {
+    this.setData({
+      type: e.currentTarget.dataset.value
+    })
+    this.getSchoolInfo()
+  }
+})
